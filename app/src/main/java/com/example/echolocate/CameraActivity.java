85595cfbaa2021db9e4c15c81c4839baf24e81dd
Alexander Sun <technoal9@gmail.com>
2020-03-30 16:28:59 -0400

    attmepting no pop up

--- app/src/main/java/com/example/echolocate/CameraActivity.java
+++ app/src/main/java/com/example/echolocate/CameraActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.CollationElementIterator;
 import java.util.ArrayList;
 import java.util.Locale;
 
@@ -34,17 +37,22 @@ public class CameraActivity extends AppCompatActivity {
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
 
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, SPEECH_TO_TEXT_REQUEST_CODE);
-        } else {
-            Toast.makeText(this, "unsupported", Toast.LENGTH_SHORT).show();
-        }
+        BypassRecognitionListener listener = new BypassRecognitionListener();
+        listener.speechTTText = this.speechTTText;
+        SpeechRecognizer sr = SpeechRecognizer.createSpeechRecognizer(this);
+        sr.setRecognitionListener(listener);
+        sr.startListening(intent);
+
+//        if (intent.resolveActivity(getPackageManager()) != null) {
+//            startActivityForResult(intent, SPEECH_TO_TEXT_REQUEST_CODE);
+//        } else {
+//            Toast.makeText(this, "unsupported", Toast.LENGTH_SHORT).show();
+//        }
     }
 
     @Override
     protected void onActivityResult ( int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
-
         switch (requestCode) {
             case SPEECH_TO_TEXT_REQUEST_CODE:
                 if (resultCode == RESULT_OK && data != null) {
@@ -54,4 +62,73 @@ public class CameraActivity extends AppCompatActivity {
                 break;
         }
     }
+
+    static public class BypassRecognitionListener implements RecognitionListener {
+        private static final String TAG = "RecognitionListener";
+        private Object TranslatorUtil;
+        private TextView speechTTText;
+        private SpeechRecognizer speechRecognizer;
+        private Intent SpeechRecognizerIntent;
+
+        BypassRecognitionListener(){
+
+        }
+        @Override
+        public void onBeginningOfSpeech()
+        {
+            //Log.d(TAG, "onBeginingOfSpeech");
+        }
+
+        @Override
+        public void onBufferReceived(byte[] buffer)
+        {
+        }
+
+        @Override
+        public void onEndOfSpeech()
+        {
+            //Log.d(TAG, "onEndOfSpeech");
+        }
+
+        @Override
+        public void onError(int error)
+        {
+            speechRecognizer.startListening(SpeechRecognizerIntent);
+            //Log.d(TAG, "error = " + error);
+        }
+
+        @Override
+        public void onEvent(int eventType, Bundle params)
+        {
+
+        }
+
+        @Override
+        public void onPartialResults(Bundle partialResults)
+        {
+
+        }
+
+        @Override
+        public void onReadyForSpeech(Bundle params)
+        {
+            Log.d(TAG, "onReadyForSpeech"); //$NON-NLS-1$
+        }
+
+        @Override
+        public void onResults(Bundle results)
+        {
+            //Log.d(TAG, "onResults"); //$NON-NLS-1$
+            ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            // matches are the return values of speech recognition engine
+            // Use these values for whatever you wish to do
+            speechTTText.setText(matches.get(0));
+        }
+
+        @Override
+        public void onRmsChanged(float rmsdB)
+        {
+        }
+    }
+
 }
